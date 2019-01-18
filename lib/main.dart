31fea8447426d6497b36aaf93cc6d0a1c3bdd457
Abdul Halim <abdul.halim@bs-23.net>
2019-01-18 20:30:33 +0600

    adding tappable icon

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class RandomWordsState extends State<RandomWords> {
 
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 19.0);
+  final _saved = new Set<WordPair>();
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -34,11 +35,25 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
